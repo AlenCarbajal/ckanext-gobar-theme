4ae5b30561645fd26f9d91360883a9ce0b9d92d0
AlenCarbajal <alencarbajal@gmail.com>
2025-12-07 20:46:52 -0300

    cambio en setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    name='ckanext-gobar-theme',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'ckan>=2.11',
+    ],
     entry_points={
         'ckan.plugins': [
             'gobar_theme=ckanext.gobar_theme.plugin:GobarThemePlugin'
